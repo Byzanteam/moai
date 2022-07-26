018deca10b766d91ed24c3fbee90e268117e97a4
CptBreeza <cbasicok@gmail.com>
2022-07-26 16:48:24 +0800

    Build Core.Library.Number

---
+++ lib/jet_exp/core/library/number.ex
@@ -0,0 +1,175 @@
+defmodule JetExp.Core.Library.Number do
+  @moduledoc false
+
+  @doc """
+  Sums up all numbers in a list. Returns 0 for empty list.
+
+  ## Examples
+
+    iex> n_sum_a([1, 2, 3])
+    {:ok, 6}
+
+    iex> n_sum_a([])
+    {:ok, 0}
+  """
+  @spec n_sum_a([number()]) :: {:ok, number()}
+  def n_sum_a(numbers) do
+    {:ok, List.foldr(numbers, 0, &+/2)}
+  end
+
+  @doc """
+  Multiplies all numbers in a list. Returns 1 for empty list.
+
+  ## Examples
+
+    iex> n_product_a([1, 2, 3])
+    {:ok, 6}
+
+    iex> n_product_a([])
+    {:ok, 1}
+  """
+  @spec n_product_a([number()]) :: {:ok, number()}
+  def n_product_a(numbers) do
+    {:ok, List.foldr(numbers, 1, &*/2)}
+  end
+
+  @doc """
+  Truncates floats to integers.
+
+  ## Examples
+
+    iex> n_truncate(1.3)
+    {:ok, 1}
+
+    iex> n_truncate(1)
+    {:ok, 1}
+  """
+  @spec n_truncate(number()) :: {:ok, integer()}
+  def n_truncate(number) do
+    {:ok, trunc(number)}
+  end
+
+  @doc """
+  Rounds a number to an arbitrary number of fractional digits.
+
+  ## Examples
+
+    iex> n_round(3.1415, 0)
+    {:ok, 3.0}
+
+    iex> n_round(3.1415, 3)
+    {:ok, 3.142}
+
+    iex> n_round(3, 1)
+    {:ok, 3}
+  """
+  @spec n_round(value :: number(), precision :: non_neg_integer()) :: {:ok, number()}
+  def n_round(number, _precision) when is_integer(number), do: {:ok, number}
+
+  def n_round(number, precision) do
+    {:ok, Float.round(number, precision)}
+  end
+
+  @doc """
+  Rounds a float to the largest number less than or equal to num.
+
+  ## Examples
+
+    iex> n_floor(3.1415, 0)
+    {:ok, 3.0}
+
+    iex> n_floor(3.1415, 3)
+    {:ok, 3.141}
+
+    iex> n_floor(3, 1)
+    {:ok, 3}
+  """
+  @spec n_floor(value :: number(), precision :: non_neg_integer()) :: {:ok, number()}
+  def n_floor(number, _precision) when is_integer(number), do: {:ok, number}
+
+  def n_floor(number, precision) do
+    {:ok, Float.floor(number, precision)}
+  end
+
+  @doc """
+  Rounds a float to the largest number less than or equal to num.
+
+  ## Examples
+
+    iex> n_ceil(3.1415, 0)
+    {:ok, 4.0}
+
+    iex> n_ceil(3.1415, 3)
+    {:ok, 3.142}
+
+    iex> n_ceil(3, 1)
+    {:ok, 3}
+  """
+  @spec n_ceil(value :: number(), precision :: non_neg_integer()) :: {:ok, number()}
+  def n_ceil(number, _precision) when is_integer(number), do: {:ok, number}
+
+  def n_ceil(number, precision) do
+    {:ok, Float.ceil(number, precision)}
+  end
+
+  @doc """
+  Parses a binary into a number.
+
+  ## Examples
+
+    iex> n_parse_string("3.1415")
+    {:ok, 3.1415}
+
+    iex> n_parse_string("3.0")
+    {:ok, 3.0}
+
+    iex> n_parse_string("3")
+    {:ok, 3}
+
+    iex> n_parse_string("foobar")
+    {:ok, nil}
+  """
+  @spec n_parse_string(String.t()) :: {:ok, number() | nil}
+  def n_parse_string(string) do
+    with(
+      :error <- parse_integer(string),
+      :error <- parse_float(string)
+    ) do
+      {:ok, nil}
+    end
+  end
+
+  defp parse_integer(string) do
+    try do
+      {:ok, String.to_integer(string)}
+    rescue
+      ArgumentError ->
+        :error
+    end
+  end
+
+  defp parse_float(string) do
+    try do
+      {:ok, String.to_float(string)}
+    rescue
+      ArgumentError ->
+        :error
+    end
+  end
+
+  @doc """
+  Converts a number to string.
+
+  ## Examples
+
+    iex> n_to_string(1)
+    {:ok, "1"}
+
+    iex> n_to_string(1.1)
+    {:ok, "1.1"}
+  """
+  @spec n_to_string(number()) :: {:ok, String.t()}
+  def n_to_string(number) do
+    {:ok, to_string(number)}
+  end
+end
---
+++ test/jet_exp/core/library/number_test.exs
@@ -0,0 +1,4 @@
+defmodule JetExp.Core.Library.NumberTest do
+  use ExUnit.Case
+  doctest JetExp.Core.Library.Number, import: true, tags: :unit
+end
